3c71d64c1dbf7e6ca122a98ca87b60ec4c757bfa
Rafael Costa <gigijanjao@gmail.com>
2017-09-26 15:24:10 -0400

    Home page frontend

---
+++ MyHealthyFinances/Content/scr.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".homeFadeInNoDelay").fadeIn(1000);
+    $(".homeFadeIn1").fadeIn(2000);
+    $(".homeFadeIn2").delay(1000).fadeIn(1000);
+    $(".homeSlideDownNoDelay").slideDown(800);
+    $(".homeSlideDown1").delay(2000).slideDown(700);
+    $(".homeSlideDown2").delay(2700).slideDown(700);
+    $(".homeSlideDown3").delay(3400).slideDown(700);
+    $("footer").delay(5100).fadeIn(1000);
+});
+
+
+
+
