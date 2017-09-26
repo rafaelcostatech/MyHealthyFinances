c1caf9f98ccf23c8e860669b57ac7ccfbfda1e1d
Rafael Costa <gigijanjao@gmail.com>
2017-09-26 16:14:06 -0400

    About FrontEnd

---
+++ MyHealthyFinances/Controllers/InfosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyHealthyFinances.Controllers
+{
+    public class InfosController : Controller
+    {
+        // GET: Infos
+        public ActionResult System()
+        {
+            return View();
+        }
+
+        public ActionResult Finances()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Tips()
+        {
+            return View();
+        }
+    }
+}
